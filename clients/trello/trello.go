1f6a09bd79bb04246aedf9b9143ffd3e1363ce6a
Jake Sutton <jake.m.sutton@gmail.com>
2016-04-30 09:24:07 -0600

    finished Trello client

--- clients/trello/trello.go
+++ clients/trello/trello.go
@@ -3,6 +3,9 @@ package trello
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 type Client struct {
@@ -12,53 +15,121 @@ type Client struct {
 	BoardName string
 }
 
-type userResponse struct {
-	//TODO
+type board struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
 }
 
-type listsResponse struct {
-	//TODO
+type card struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
 }
 
-func (c Client) boardId(key, token, boardName string) (string, error) {
-	//TODO: query /1/members/me, extract id from boardName
+type list struct {
+	Id    string `json:"id"`
+	Name  string `json:"name"`
+	Cards []card `json:"cards"`
 }
 
-func (c Client) cardNames(key, token, boardId, listName string) ([]string, error) {
-	//TODO: query /1/boards/[boardId]/lists, extract card names from listName
-}
-
-func (c Client) Tasks(key, token, boardName, listName string) ([]string, error) {
-	if key == "" {
-		key = c.Key
-		if key == "" {
-			return nil, errors.New("No Todoist application key supplied")
-		}
-	}
-	if token == "" {
-		token = c.Token
-		if token == "" {
-			return nil, errors.New("No Todoist auth token supplied")
-		}
-	}
-	if boardName == "" {
-		boardName = c.BoardName
-		if boardName == "" {
-			return nil, errors.New("No Todoist board name supplied")
-		}
-	}
-
-	boardId, err := c.boardId(key, token, boardName)
+func (c Client) get(query string) ([]byte, error) {
+	request := c.Url + query + "&key=" + c.Key + "&token=" + c.Token
+	response, err := http.Get(request)
 	if err != nil {
 		return nil, err
 	}
-	tasks, err := c.cardNames(key, token, boardId, listName)
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	response.Body.Close()
+
+	if response.StatusCode != 200 {
+		return nil, errors.New(fmt.Sprintf(
+			"Bad API response for \"%s\":\n"+
+				"Status: %s\n"+
+				"Body: %s\n",
+			request, response.Status, string(body)))
+	}
+
+	return body, nil
+}
+
+
+func (c Client) boardId() (string, error) {
+	body, err := c.get("members/me/boards?fields=name")
+	if err != nil {
+		return "", err
+	}
+
+	boards := make([]board, 0)
+	err = json.Unmarshal(body, &boards)
+	if err != nil {
+		return "", err
+	}
+
+	for _, board := range boards {
+		if board.Name == c.BoardName {
+			return board.Id, nil
+		}
+	}
+
+	return "", errors.New("Board named \"" + c.BoardName + "\" not found.")
+}
+
+func (c Client) cardNames(boardId, listName string) ([]string, error) {
+	body, err := c.get("boards/"+boardId+"/lists?fields=name&cards=all&card_fields=name")
+	if err != nil {
+		return nil, err
+	}
+
+	lists := make([]list, 0)
+	err = json.Unmarshal(body, &lists)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, l := range lists {
+		if l.Name == listName {
+			tasks := make([]string, len(l.Cards))
+			for i, c := range l.Cards {
+				tasks[i] = c.Name
+			}
+			return tasks, nil
+		}
+	}
+
+	return nil, errors.New("List named \"" + listName + "\" not found.")
+}
+
+func New(key, token, boardName string) Client {
+	return Client{
+		Url: "https://api.trello.com/1/",
+		Key: key,
+		Token: token,
+		BoardName: boardName,
+	}
+}
+
+func (c Client) Tasks(key, token, boardName, listName string) ([]string, error) {
+	newClient := c
+	if key != "" {
+		newClient.Key = key
+	}
+	if token != "" {
+		newClient.Token = token
+	}
+	if boardName != "" {
+		newClient.BoardName = boardName
+	}
+
+	boardId, err := newClient.boardId()
+	if err != nil {
+		return nil, err
+	}
+	tasks, err := newClient.cardNames(boardId, listName)
 	if err != nil {
 		return nil, err
 	}
 	return tasks, nil
-}
-
-func (c Client) Tasks(listName string) ([]string, error) {
-	return c.Tasks("", "", "", listName)
 }
---
+++ clients/trello/trello_test.go
@@ -0,0 +1,64 @@
+package trello
+
+import (
+	"os"
+	"testing"
+)
+
+/* This test expects a board named "Todo Test" with a list named "Test" 
+ * with (only) cards named "foo", "bar", and "baz", in that order.
+ */
+func TestGetList(t *testing.T) {
+	key := os.Getenv("TEST_TRELLO_KEY")
+	if key == "" {
+		t.Fatal("missing environment variable TEST_TRELLO_KEY")
+	}
+	token := os.Getenv("TEST_TRELLO_TOKEN")
+	if token == "" {
+		t.Fatal("missing environment variable TEST_TRELLO_TOKEN")
+	}
+
+	boardName := "Todo Test"
+	listName := "Test"
+	correctTasks := []string{"foo", "bar", "baz"}
+
+	t.Log("testing with constructor params")
+	c := New(key, token, boardName)
+	tasks, err := c.Tasks("", "", "", listName)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(tasks) != len(correctTasks) {
+		t.Logf("tasks wrong: expected %v, got %v", correctTasks, tasks)
+		t.Fail()
+	} else {
+		for i, task := range correctTasks {
+			if tasks[i] != task {
+				t.Logf("tasks wrong: expected %v, got %v",
+					correctTasks, tasks)
+				t.Fail()
+				break
+			}
+		}
+	}
+
+	t.Log("testing with method params")
+	c = New("override", "these", "params")
+	tasks, err = c.Tasks(key, token, boardName, listName)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(tasks) != len(correctTasks) {
+		t.Logf("tasks wrong: expected %v, got %v", correctTasks, tasks)
+		t.Fail()
+	} else {
+		for i, task := range correctTasks {
+			if tasks[i] != task {
+				t.Logf("tasks wrong: expected %v, got %v",
+					correctTasks, tasks)
+				t.Fail()
+				break
+			}
+		}
+	}
+}
